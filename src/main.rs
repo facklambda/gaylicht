eb099da871d556ed94897dcb6c398a2276206e53
fack <fack>
2022-06-23 01:09:40 -0500

    added trans flag support

--- src/main.rs
+++ src/main.rs
@@ -4,37 +4,41 @@ use ws2818_rgb_led_spi_driver::encoding::encode_rgb;
 
 fn main() {
     println!("make sure you have \"SPI\" on your Pi enabled and that MOSI-Pin is connected with DIN-Pin!");
+    trans_pride();
+}
+
+fn trans_pride() {
     let mut adapter = WS28xxSpiAdapter::new("/dev/spidev0.0").unwrap();
 
     // Method 1: encode first and write in two step (preferred way; better performance)
     {
         let mut spi_encoded_rgb_bits = vec![];
         // set first three pixels to bright red, bright green and bright blue
-        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(255, 0, 0));
-        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(255, 0, 0));
-        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(255, 0, 0));
-        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(255, 0, 0));
-        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(255, 0, 0));
-        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(0, 255, 0));
-        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(0, 255, 0));
-        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(0, 255, 0));
-        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(0, 255, 0));
-        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(0, 255, 0));
-        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(0, 0, 255));
-        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(0, 0, 255));
-        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(0, 0, 255));
-        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(0, 0, 255));
-        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(0, 0, 255));
-        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(0, 255, 0));
-        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(0, 255, 0));
-        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(0, 255, 0));
-        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(0, 255, 0));
-        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(0, 255, 0));
-        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(255, 0, 0));
-        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(255, 0, 0));
-        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(255, 0, 0));
-        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(255, 0, 0));
-        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(255, 0, 0));
+        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(91, 206, 250));
+        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(91, 206, 250));
+        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(91, 206, 250));
+        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(91, 206, 250));
+        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(91, 206, 250));
+        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(245, 169, 184));
+        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(245, 169, 184));
+        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(245, 169, 184));
+        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(245, 169, 184));
+        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(245, 169, 184));
+        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(255, 255, 255));
+        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(255, 255, 255));
+        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(255, 255, 255));
+        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(255, 255, 255));
+        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(255, 255, 255));
+        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(245, 169, 184));
+        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(245, 169, 184));
+        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(245, 169, 184));
+        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(245, 169, 184));
+        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(245, 169, 184));
+        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(91, 206, 250));
+        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(91, 206, 250));
+        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(91, 206, 250));
+        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(91, 206, 250));
+        spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(91, 206, 250));
         adapter.write_encoded_rgb(&spi_encoded_rgb_bits).unwrap();
     }
 }
